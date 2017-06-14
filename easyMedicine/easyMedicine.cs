b8c22130a9b09d16709cb7eaf023db6d75e27631
Pedro Rocha <pedrocha@gmail.com>
2017-06-14 23:41:13 +0100

    fix start

--- easyMedicine/easyMedicine.cs
+++ easyMedicine/easyMedicine.cs
@@ -1,39 +1,43 @@
 ﻿using System;
-
+using Microsoft.Azure.Mobile;
+using Microsoft.Azure.Mobile.Analytics;
+using Microsoft.Azure.Mobile.Crashes;
 using Xamarin.Forms;
 
 namespace easyMedicine
 {
 
 
-	public class App : Application
-	{
-		public Bootstrapper BootStrapper;
+    public class App : Application
+    {
+        public Bootstrapper BootStrapper;
 
-		public App ()
-		{
-			//GmsDirection.Init ("AIzaSyAzlgYgYEQFVR-j2t6auJuXUoF3Y6s1Efk");
-			Styles.LoadStyles ();
-			this.BootStrapper = new Bootstrapper (this);
+        public App()
+        {
+            //GmsDirection.Init ("AIzaSyAzlgYgYEQFVR-j2t6auJuXUoF3Y6s1Efk");
+            Styles.LoadStyles();
+            this.BootStrapper = new Bootstrapper(this);
 
 
-			//System.AppDomain.UnhandledException+=
-		}
+            //System.AppDomain.UnhandledException+=
+        }
 
-		protected override void OnStart ()
-		{
-			// Handle when your app starts
-		}
+        protected override void OnStart()
+        {
+            MobileCenter.Start("ios=f4b28f29-a8b8-4197-870e-35aec416753a;",
+                   typeof(Analytics), typeof(Crashes));
+            // Handle when your app starts
+        }
 
-		protected override void OnSleep ()
-		{
-			// Handle when your app sleeps
-		}
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
 
-		protected override void OnResume ()
-		{
-			// Handle when your app resumes
-		}
-	}
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
 }
 
--- iOS/AppDelegate.cs
+++ iOS/AppDelegate.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-
+using Microsoft.Azure.Mobile;
+using Microsoft.Azure.Mobile.Analytics;
+using Microsoft.Azure.Mobile.Crashes;
 using Foundation;
 using UIKit;
 using easyMedicine.iOS.Data;
@@ -10,51 +12,54 @@ using System.IO;
 
 namespace easyMedicine.iOS
 {
-	[Register ("AppDelegate")]
-	public partial class AppDelegate : global::Xamarin.Forms.Platform.iOS.FormsApplicationDelegate
-	{
-		public override bool FinishedLaunching (UIApplication app, NSDictionary options)
-		{
-			/*
+    [Register("AppDelegate")]
+    public partial class AppDelegate : global::Xamarin.Forms.Platform.iOS.FormsApplicationDelegate
+    {
+        public override bool FinishedLaunching(UIApplication app, NSDictionary options)
+        {
+            //MobileCenter.Start("f4b28f29-a8b8-4197-870e-35aec416753a",
+            //typeof(Analytics), typeof(Crashes));
+
+            /*
 			var appdir = NSBundle.MainBundle.ResourcePath;
 			var seedFile = Path.Combine (appdir, "data.sqlite");
 			if (!File.Exists (Database.DatabaseFilePath))
 				File.Copy (seedFile, Database.DatabaseFilePath);
 */
-			global::Xamarin.Forms.Forms.Init ();
+            global::Xamarin.Forms.Forms.Init();
 
-			var app1 = new App ();
+            var app1 = new App();
 
-			RegisterComponents (app1.BootStrapper);
-
-
-			LoadApplication (app1);
-
-			UINavigationBar.Appearance.SetTitleTextAttributes(new UITextAttributes()
-			{
-				Font = UIFont.FromName("Avenir-Book", 20),
-			});
+            RegisterComponents(app1.BootStrapper);
 
 
+            LoadApplication(app1);
 
-			UITabBarItem.Appearance.SetTitleTextAttributes(
-				new UITextAttributes()
-				{
-					Font = UIFont.FromName("Avenir-Book", 10),
-				}, UIControlState.Normal);
-			//UISearchBar.Appearance. = UIColor.Red;
+            UINavigationBar.Appearance.SetTitleTextAttributes(new UITextAttributes()
+            {
+                Font = UIFont.FromName("Avenir-Book", 20),
+            });
 
-			return base.FinishedLaunching (app, options);
-		}
 
-		private void RegisterComponents (Bootstrapper boot)
-		{
-			boot.RegisterPlatformService<ISQLite> (new IOSSQLite ());
-			/*boot.RegisterPlatformService<ILocalizeService> (new IosLocalizeService ());
+
+            UITabBarItem.Appearance.SetTitleTextAttributes(
+                new UITextAttributes()
+                {
+                    Font = UIFont.FromName("Avenir-Book", 10),
+                }, UIControlState.Normal);
+            //UISearchBar.Appearance. = UIColor.Red;
+
+            return base.FinishedLaunching(app, options);
+        }
+
+        private void RegisterComponents(Bootstrapper boot)
+        {
+            boot.RegisterPlatformService<ISQLite>(new IOSSQLite());
+            /*boot.RegisterPlatformService<ILocalizeService> (new IosLocalizeService ());
 			boot.RegisterPlatformService<ISocketIOService> (new IOSSocketIOService ());*/
-			boot.Start ();
-		}
-	}
+            boot.Start();
+        }
+    }
 
 
 }
