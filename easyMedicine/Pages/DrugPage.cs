f0e050e2349c6438c8c15c64a6d3f3c2dae7401c
Pedro Rocha <pedrocha@gmail.com>
2017-09-04 00:08:36 +0100

    list view fixed

--- easyMedicine/Pages/DrugPage.cs
+++ easyMedicine/Pages/DrugPage.cs
@@ -14,6 +14,135 @@ namespace easyMedicine
             }
         }
 
+        //public DrugPage(DrugPageModel model) : base(model)
+        //{
+        //    this.BindingContext = Model;
+        //    this.SetBinding(Page.TitleProperty, "Drug.Name");
+
+
+        //    var favButton = new ToolbarItem
+        //    {
+        //        BindingContext = Model,
+        //    };
+        //    favButton.SetBinding(ToolbarItem.IconProperty, DrugPageModel.FavouriteIconPropertyName);
+        //    favButton.SetBinding(ToolbarItem.CommandProperty, DrugPageModel.ChangeFavouriteStatusPropertyName);
+
+        //    this.ToolbarItems.Add(favButton);
+
+        //    //this.Padding = new Thickness(5);
+
+
+        //    var layout = new StackLayout()
+        //    {
+        //        HorizontalOptions = LayoutOptions.FillAndExpand,
+        //        VerticalOptions = LayoutOptions.FillAndExpand,
+        //    };
+
+        //    var layoutHeader = new StackLayout()
+        //    {
+        //        HorizontalOptions = LayoutOptions.FillAndExpand,
+        //        VerticalOptions = LayoutOptions.FillAndExpand,
+        //        Padding = new Thickness(5),
+        //    };
+
+        //    var lbtname = new LabelValue("Nome", "Drug.Name");
+        //    layoutHeader.Children.Add(lbtname);
+        //    //var lbtbrand = new LabelValue("Marca Comercial", "Drug.CommercialBrand");
+        //    //layoutHeader.Children.Add(lbtbrand);
+        //    var lbtConterIndications = new LabelValue("Contra-Indicações", "Drug.ConterIndications");
+        //    layoutHeader.Children.Add(lbtConterIndications);
+        //    var lbtSecEffects = new LabelValue("Efeitos Secundários", "Drug.SecondaryEfects");
+        //    layoutHeader.Children.Add(lbtSecEffects);
+        //    var lbtPresentation = new LabelValue("Apresentação", "Drug.Presentation");
+        //    layoutHeader.Children.Add(lbtPresentation);
+        //    var lbtCommercialBrands = new LabelValue("Marcas Comerciais", "Drug.ComercialBrands");
+        //    layoutHeader.Children.Add(lbtCommercialBrands);
+        //    var lbtObs = new LabelValue("Outros Dados", "Drug.Obs");
+        //    layoutHeader.Children.Add(lbtObs);
+
+
+        //    layout.Children.Add(layoutHeader);
+
+        //    var lstView = new ListView()
+        //    {
+        //        HorizontalOptions = LayoutOptions.FillAndExpand,
+        //        VerticalOptions = LayoutOptions.FillAndExpand,
+        //    };
+        //    lstView.HasUnevenRows = true;
+        //    lstView.IsGroupingEnabled = true;
+        //    //lstView.GroupDisplayBinding = new Binding("Description");
+        //    //lstView.GroupShortNameBinding = new Binding("ShortName");
+        //    lstView.SetBinding(ListView.ItemsSourceProperty, DrugPageModel.IndicationsPropertyName);
+        //    lstView.SeparatorColor = Styles.BLUE_COLOR;
+        //    lstView.ItemTemplate = new DataTemplate(typeof(DoseCell));
+        //    lstView.ItemTemplate.SetBinding(DoseCell.ViaProperty, "IdVia");
+        //    lstView.ItemTemplate.SetBinding(DoseCell.PedDoseProperty, "PediatricDose");
+        //    lstView.ItemTemplate.SetBinding(DoseCell.PedDoseUnityProperty, "IdUnityPediatricDose");
+        //    lstView.ItemTemplate.SetBinding(DoseCell.AdultDoseProperty, "AdultDose");
+        //    lstView.ItemTemplate.SetBinding(DoseCell.AdultDoseUnityProperty, "IdUnityAdultDose");
+
+        //    lstView.ItemTemplate.SetBinding(DoseCell.TakesPerDayProperty, "TakesPerDay");
+        //    lstView.ItemTemplate.SetBinding(DoseCell.MaxDosePerDayProperty, "MaxDosePerDay");
+        //    lstView.ItemTemplate.SetBinding(DoseCell.MaxDosePerDayUnityProperty, "IdUnityMaxDosePerDay");
+        //    lstView.ItemTemplate.SetBinding(DoseCell.ObsProperty, "Obs");
+        //    lstView.ItemTemplate.SetBinding(DoseCell.DescriptionProperty, "Description");
+
+        //    lstView.GroupHeaderTemplate = new DataTemplate(() =>
+        //    {
+        //        var ghlayout = new StackLayout()
+        //        {
+        //            Padding = new Thickness(5),
+        //            HorizontalOptions = LayoutOptions.FillAndExpand,
+        //            VerticalOptions = LayoutOptions.Fill,
+        //            BackgroundColor = Styles.BLUE_COLOR,
+        //            Orientation = StackOrientation.Horizontal
+
+        //        };
+
+        //        var hdrLayout = new Label()
+        //        {
+        //            LineBreakMode = LineBreakMode.TailTruncation,
+        //            Style = (Style)Application.Current.Resources[Styles.Style_LabelMediumBackgroundStyle],
+        //        };
+        //        hdrLayout.SetBinding(Label.TextProperty, "Description");
+
+        //        ghlayout.Children.Add(hdrLayout);
+
+        //        var img = new Image()
+        //        {
+        //            HorizontalOptions = LayoutOptions.EndAndExpand,
+        //            Source = "ic_info_outline_white_48pt.png"
+        //        };
+
+        //        var tapImage = new TapGestureRecognizer();
+        //        //Binding events  
+        //        tapImage.Tapped += tapImage_Tapped;
+        //        //Associating tap events to the image buttons  
+        //        img.GestureRecognizers.Add(tapImage);
+        //        void tapImage_Tapped(object sender, EventArgs e)
+        //        {
+        //            // handle the tap 
+
+        //            DisplayAlert("Indicação", hdrLayout.Text, "OK");
+        //        }
+
+        //        ghlayout.Children.Add(img);
+
+
+        //        return new ViewCell() { View = ghlayout, };
+        //    });
+
+        //    layout.Children.Add(lstView);
+
+        //    var scroll = new ScrollView()
+        //    {
+        //    };
+        //    scroll.Content = layout;
+
+        //    Content = scroll;
+
+        //}
+
         public DrugPage(DrugPageModel model) : base(model)
         {
             this.BindingContext = Model;
@@ -32,11 +161,11 @@ namespace easyMedicine
             //this.Padding = new Thickness(5);
 
 
-            var layout = new StackLayout()
-            {
-                HorizontalOptions = LayoutOptions.FillAndExpand,
-                VerticalOptions = LayoutOptions.FillAndExpand,
-            };
+            //var layout = new StackLayout()
+            //{
+            //	HorizontalOptions = LayoutOptions.FillAndExpand,
+            //	VerticalOptions = LayoutOptions.FillAndExpand,
+            //};
 
             var layoutHeader = new StackLayout()
             {
@@ -61,7 +190,7 @@ namespace easyMedicine
             layoutHeader.Children.Add(lbtObs);
 
 
-            layout.Children.Add(layoutHeader);
+            //layout.Children.Add(layoutHeader);
 
             var lstView = new ListView()
             {
@@ -132,14 +261,11 @@ namespace easyMedicine
                 return new ViewCell() { View = ghlayout, };
             });
 
-            layout.Children.Add(lstView);
+            lstView.Header = layoutHeader;
 
-            var scroll = new ScrollView()
-            {
-            };
-            scroll.Content = layout;
+            //layout.Children.Add(lstView);
 
-            Content = scroll;
+            Content = lstView;
 
         }
     }
